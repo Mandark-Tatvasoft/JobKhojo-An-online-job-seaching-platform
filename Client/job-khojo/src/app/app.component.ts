409d7285af484e6fd406874f7dd545defc7067f7
Mandark <mandark115@gmail.com>
2024-06-04 00:27:28 +0530

    navbar done

---
+++ Client/job-khojo/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'job-khojo';
+}
